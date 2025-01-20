127fa3d5f6b7c1cd20b25690edc62ca600c0bfdc
Abdelrhman khaled Mohammed <124658552+AbdelrhmanKhaled76@users.noreply.github.com>
2025-01-20 21:44:10 +0200

    feat:register-page

--- Frontend/src/app/pages/register/register.component.ts
+++ Frontend/src/app/pages/register/register.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  registerForm : FormGroup = new FormGroup({
+    image : new FormControl(''),
+    email : new FormControl(''),
+    userName : new FormControl(''),
+    password: new FormControl(''),
+    repassword : new FormControl(''),
+    phone : new FormControl(''),
+    userType : new FormControl('student')
+  });
 
+  registerSubmit(){
+    if(this.registerForm.valid){
+      console.log(this.registerForm.value);
+    }
+    else {
+      this.registerForm.markAllAsTouched();
+    }
+  }
 }
