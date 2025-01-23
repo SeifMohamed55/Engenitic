e9296eb78a360d1f8066e92fa7df7b7fbccc8b08
Abdelrhman khaled Mohammed <124658552+AbdelrhmanKhaled76@users.noreply.github.com>
2025-01-24 01:48:48 +0200

    feat:course-page-is-done

--- Frontend/src/app/pages/course/course.component.ts
+++ Frontend/src/app/pages/course/course.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-course',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './course.component.html',
   styleUrl: './course.component.scss'
 })
-export class CourseComponent {
+export class CourseComponent implements OnInit {
+  
 
+  collectionId !: number | null;
+
+  constructor(private _ActivatedRoute : ActivatedRoute){
+
+  }
+
+  ngOnInit(): void {
+    this.collectionId = Number.parseInt(this._ActivatedRoute.snapshot.paramMap.get('collection')!);
+    console.log(this.collectionId);
+  }
+
+  
 }
