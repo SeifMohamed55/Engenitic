163cdb586c0a7a7148db36fa5b599e41aa3da2c9
Abdelrhman khaled Mohammed <124658552+AbdelrhmanKhaled76@users.noreply.github.com>
2025-01-26 21:52:50 +0200

    feat: listening page completed

--- Frontend/src/app/pages/listening/listening.component.ts
+++ Frontend/src/app/pages/listening/listening.component.ts
@@ -1,11 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-listening',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './listening.component.html',
   styleUrl: './listening.component.scss'
 })
 export class ListeningComponent {
+  listeningForm : FormGroup = new FormGroup ({
+    sentence : new FormControl('', [Validators.required]),
+  });
 
+  isDisabled : boolean = false;
+
+  handleSubmit() : void{
+    this.isDisabled = true;
+    if(this.listeningForm.valid){
+      console.log(this.listeningForm.value);
+      this.isDisabled = false;
+    }
+    else {
+      this.listeningForm.markAllAsTouched();
+      this.isDisabled = false;
+    }
+  }
 }
