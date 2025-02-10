8b2afa99f85b887e73f790d31ef25cf419e5fc97
Abdelrhman khaled Mohammed <124658552+AbdelrhmanKhaled76@users.noreply.github.com>
2025-02-11 00:18:15 +0200

    feat: preparing first service

--- Frontend/src/app/feature/users/user.service.ts
+++ Frontend/src/app/feature/users/user.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, Observable } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private _HttpClient:HttpClient) {
+
+  }
+
+  registerData(value : any) : Observable<any> 
+  {
+    this._HttpClient.post(`https://localhost/api/Authentication/register`, value)
+  };
+
+  
 }
