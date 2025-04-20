0dcbc4196926da59721f6820d0370b79533d506e
SeifMohamed55 <seifelden073@gmail.com>
2025-04-20 22:28:51 +0200

    Adding Indexes Migration

---
+++ Backend/GraduationProject/GraduationProject/Migrations/20250420202333_AddingIndexesToReview.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GraduationProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingIndexesToReview : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_UserId",
+                table: "Reviews");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "QuizId",
+                table: "UserQuizAttempts",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "QuestionId",
+                table: "UserAnswers",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AnswerId",
+                table: "UserAnswers",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_Rating",
+                table: "Reviews",
+                column: "Rating");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_UserId_CourseId",
+                table: "Reviews",
+                columns: new[] { "UserId", "CourseId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_Rating",
+                table: "Reviews");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_UserId_CourseId",
+                table: "Reviews");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "QuizId",
+                table: "UserQuizAttempts",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "QuestionId",
+                table: "UserAnswers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AnswerId",
+                table: "UserAnswers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_UserId",
+                table: "Reviews",
+                column: "UserId");
+        }
+    }
+}
