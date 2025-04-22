fd05476494ffcb7e01b793b7227b7eee659077da
Abdelrhman khaled Mohammed <124658552+AbdelrhmanKhaled76@users.noreply.github.com>
2025-04-23 01:22:32 +0200

    feat:redirection page

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -25,7 +25,7 @@ export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   {
-    path: 'redirection_page',
+    path: 'redirection-page',
     component: RedirectionPageComponent,
     data: { roles: ['student', 'instructor', 'admin'] },
     canActivate: [rolesGuard],
